fadf333293c4efe8c06a957e499cf7d6d21f3a0f
Udhayabalachandar <udhaya.chandrasekaran@5datainc.com>
2022-01-18 23:42:56 +0100

    tech(base): add spring boot Initial Setup code

---
+++ src/test/java/com/clone/linkedin/LinkedinCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
