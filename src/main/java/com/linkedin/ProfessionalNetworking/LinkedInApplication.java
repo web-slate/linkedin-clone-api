1fb2d5f9d900f716994216a2bf45f2d5a866b069
Udhayabalachandar <udhaya.chandrasekaran@5datainc.com>
2022-01-19 09:19:38 +0100

    tech(base): Updated for Review Comments - spring boot Initial Setup code

--- src/main/java/com/linkedin/ProfessionalNetworking/LinkedInApplication.java
+++ src/main/java/com/linkedin/ProfessionalNetworking/LinkedInApplication.java
@@ -1,7 +1,10 @@
 package com.linkedin.ProfessionalNetworking;
 
 import org.springframework.boot.SpringApplication;
+<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+=======
+>>>>>>> dfac09b (tech(base): Updated for Review Comments - spring boot Initial Setup code)
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
