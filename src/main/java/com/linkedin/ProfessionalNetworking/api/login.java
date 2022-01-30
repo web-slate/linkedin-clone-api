a99d4af6ab61f7a96cdd9b88f39b30a8a8ae6737
udhaya28 <udhayabalachandar.chandarsekran@gmail.com>
2022-01-30 11:32:22 +0100

    tech(base): add h2 database configuration (#6)
    * tech(base): add spring boot Initial Setup code
    * tech(base): Updated for Review Comments - spring boot Initial Setup code
    * tech(base): Update Readme Document
    * tech(base): Update Readme Document
    * tech(base): add h2 database configuration.
    Co-authored-by: Udhayabalachandar <udhaya.chandrasekaran@5datainc.com>

---
+++ src/main/java/com/linkedin/ProfessionalNetworking/api/login.java
@@ -0,0 +1,13 @@
+package com.linkedin.ProfessionalNetworking.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class login {
+
+
+
+}
