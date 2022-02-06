7593064e626e6c9c921faefc7a56bddcda9376f7
Udhayabalachandar <udhaya.chandrasekaran@5datainc.com>
2022-02-06 21:18:52 +0100

    tech(base): add login api, Fetch Feed and Security & Swagger Config.

--- src/main/java/com/linkedin/ProfessionalNetworking/api/login.java
+++ src/main/java/com/linkedin/ProfessionalNetworking/api/login.java
@@ -4,26 +4,33 @@ package com.linkedin.ProfessionalNetworking.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.linkedin.ProfessionalNetworking.response.ApiResponse;
 import com.linkedin.ProfessionalNetworking.util.Constants;
-import io.netty.util.internal.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api")
 public class login {
 
     private static final Logger log = LoggerFactory.getLogger(login.class);
 
+    /**
+     * This Method is used to check the Login of the application
+     * @param userId
+     * @param password
+     * @return
+     * @throws JsonProcessingException
+     */
     @GetMapping(value = "/login")
-    public ResponseEntity<ApiResponse> createDocument(@RequestParam String userId, @RequestParam String password) throws JsonProcessingException {
+    public ResponseEntity<ApiResponse> login(@RequestParam String userId, @RequestParam String password) throws JsonProcessingException {
         log.info("Login into Linkedin Professional Networking [{}]", userId, password);
         ApiResponse apiResponse = new ApiResponse();
         //FIXME: Hardcoded values are to be replaced.
-       if(userId.equals("admin") || password.equals("admin")){
+       if(userId.equals("admin") && password.equals("admin")){
            apiResponse.setStatus(HttpStatus.OK.toString());
            apiResponse.setMessage("Success");
        }else{
