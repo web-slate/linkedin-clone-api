5d8472d102a38df12285fb41b2e485c579c88b93
udhaya28 <udhayabalachandar.chandarsekran@gmail.com>
2022-02-24 17:22:23 +0100

    fix(base) : fix cors issues (#21)
    * tech(base): add spring boot Initial Setup code
    
    * tech(base): Updated for Review Comments - spring boot Initial Setup code
    
    * tech(base): Update Readme Document
    
    * tech(base): Update Readme Document
    
    * tech(base): add h2 database configuration.
    
    * tech(base): add login api
    
    * tech(base): add login api, Fetch Feed and Security & Swagger Config.
    
    * tech(base): Update Login API and Review Comments.
    
    * tech(base): update login api with postgres connection
    
    * tech(base): Update Login Api changed to request body
    
    * tech(base): add spring boot Initial Setup code
    
    * tech(base): Updated for Review Comments - spring boot Initial Setup code
    
    * tech(base): Update Readme Document
    
    * tech(base): Update Readme Document
    
    * tech(base): add h2 database configuration.
    
    * tech(base): add login api
    
    * tech(base): add login api, Fetch Feed and Security & Swagger Config.
    
    * tech(base): Update Login API and Review Comments.
    
    * tech(base): update login api with postgres connection
    
    * tech(base): update login api
    
    * tech(base): Update for Feed
    
    * tech(base): update feed Request mapping POST to GET
    
    * tech(base): update create and get feed, connected to postgres DB
    
    * tech(base): update the CORS disabled.
    
    * tech(base): fix CORS issues
    
    Co-authored-by: Udhayabalachandar <udhaya.chandrasekaran@5datainc.com>

--- src/main/java/com/linkedin/ProfessionalNetworking/config/BaseSecurityConfiguration.java
+++ src/main/java/com/linkedin/ProfessionalNetworking/config/BaseSecurityConfiguration.java
@@ -12,9 +12,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 
-
-
-
 /**
  * This class used to Enable the Login Security
  */
@@ -25,12 +22,24 @@ public class BaseSecurityConfiguration extends WebSecurityConfigurerAdapter {
     //FIXME: Method has to change when real time security enabled.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       // http.authorizeRequests().antMatchers("/").permitAll();
+        // http.authorizeRequests().antMatchers("/").permitAll();
         http.cors().and().csrf().disable();
     }
 
 
-
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 }
