bf5cdd9f6297c5018f36b41f09e21346635901ce
Venkat.R <ramsunvtech@gmail.com>
2022-04-15 18:53:26 +0800

    docs(main): add profile table and format file

--- src/main/resources/DB/DDL/createTables.sql
+++ src/main/resources/DB/DDL/createTables.sql
@@ -1,20 +1,27 @@
 
 CREATE TABLE public.t_user_feed (
-                                    user_id varchar NOT NULL,
-                                    legalname varchar NULL,
-                                    occupation varchar NULL,
-                                    companyname varchar NULL,
-                                    feedmsg varchar NULL,
-                                    totallike varchar NULL,
-                                    totalcomment varchar NULL,
-                                    "comment" varchar NULL,
-                                    connectiontype varchar NULL,
-                                    feed_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY
+    user_id varchar NOT NULL,
+    legalname varchar NULL,
+    occupation varchar NULL,
+    companyname varchar NULL,
+    feedmsg varchar NULL,
+    totallike varchar NULL,
+    totalcomment varchar NULL,
+    "comment" varchar NULL,
+    connectiontype varchar NULL,
+    feed_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY
 );
 
 
 CREATE TABLE public.t_login (
-                                user_id varchar NOT NULL,
-                                "password" varchar NOT NULL,
-                                CONSTRAINT login_pk PRIMARY KEY (user_id)
+    user_id varchar NOT NULL,
+    "password" varchar NOT NULL,
+    CONSTRAINT login_pk PRIMARY KEY (user_id)
+);
+
+CREATE TABLE public.t_user_profile (
+    user_id varchar NOT NULL,
+    first_name varchar NULL,
+    last_name varchar NULL,
+    profile_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY
 );
